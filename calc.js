d6206717cce23a00be642dcda52a7ad8700d182e
Katalout <253144538+Katalout@users.noreply.github.com>
2026-02-26 14:18:16 +0100

    Make 1+1=2 work and display it correctly
    Num1 can already be multidigital.
    
    Num2 not yet.
    
    	modified:   calc.js
    	modified:   index.html

--- calc.js
+++ calc.js
@@ -2,7 +2,7 @@
 
 let num1;
 let num2;
-let operator;
+let operator = false;
 
 const operate = function (num1, operator, num2) {
   return operator(num1, num2);
@@ -24,6 +24,10 @@ const multiply = function (array) {
   return array.reduce((prev, current) => prev * current)
 };
 
+const divide = function (a, b) {
+  return a / b;
+}
+
 const power = function (base, exponent) {
   return base ** exponent;
 };
@@ -40,11 +44,75 @@ const factorial = function (num) {
 const display = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".number");
 
-numberButtons.forEach((button) => {
+/*numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
     let type = button.textContent;
     display.textContent += type;
     num1 = +display.textContent;
     console.log(num1);
   });
+});*/
+
+const operators = document.querySelectorAll(".operator");
+
+// make objects to store button+operator function together?
+//or just do them one by one, its only 4....
+
+//when i push button, let operator be that function
+
+const addButton = document.querySelector(".add");
+
+
+// how to get num2? nest the three handlers?
+//apparently not
+
+// nest num2 creation in if?
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+
+    if (operator) {
+      const span = document.createElement("span");
+      span.textContent += button.textContent;
+      display.appendChild(span);
+      num2 = +span.textContent;
+      console.log("num2 is " + num2);
+    } else {
+
+      let type = button.textContent;
+      display.textContent += type;
+      num1 = +display.textContent;
+      console.log("num1 is " + num1);
+      addButton.addEventListener("click", () => {
+        operator = add;
+        console.log(operator);
+        const span = document.createElement("span");
+        span.textContent += addButton.textContent;
+        display.appendChild(span);
+
+      });
+    }
+  });
 });
+
+//nah 1jegyü szamokkal müködik az assignment meg a displayeles is, nice.
+// akkor most kene, hogy az egyenlösegjellel elvegezze a müveletet.
+
+const equals = document.querySelector(".equal");
+
+equals.addEventListener("click", () => {
+  let result = operator(num1, num2);
+  console.log(`equals ${result}`);
+  // ok, megcsinalja az egyenletet. akkor most irja is ki az eredemnyt
+  const span = document.createElement("span");
+  span.textContent = `=${result}`;
+  display.appendChild(span);
+});
+//ok, ki is irja az eredmenyt.
+
+//akkor most kene tudni többjegyü szamot csinalni num1-be--varj ez mar megvan lol
+//tudjon több jegyü szamot csinalni num2-be
+
+
+
+//majd a pluszjelbe kene rakni "once" cuccot hogy csak egyszer irja kiiiiii
